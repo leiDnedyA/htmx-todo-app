9c07348d146fa512795c6c647c663dc672cdadd3
leiDnedyA <aydendiel@gmail.com>
2023-09-27 22:30:01 -0400

    added sqlalchemy

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, send_file
+from sqlalchemy import create_engine, text
 import random
 
 app = Flask(__name__)
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 
 phrases = ["Live long and prosper", "Rawr", "Hey gurrrrrrl"]
 def random_phrase():
 	return phrases[random.randrange(len(phrases))]
+
+def start_engine():
+	with engine.connect() as conn:
+		result = conn.execute(text("select 'hello world'"))
+		print(result.all())
 
 @app.route('/')
 def home():
@@ -16,4 +23,5 @@ def getPhrase():
 	return f'<h2>{random_phrase()}</h2>'
 
 if __name__ == "__main__":
+	start_engine()
 	app.run(port=8080, debug=True)
